d5f01c014a8098d2e7c598a748a4ef14b8446ed7
Rob Wood <rdrwood@gmail.com>
2018-07-11 15:02:18 -0700

    designed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,48 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace DiceRoller
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Die dice1 = new Die();
+            Die dice2 = new Die();
+            Die dice3 = new Die();
+            Die dice4 = new Die();
+            Die dice5 = new Die();
+            
+
+            Console.WriteLine(dice1.Value);
+            Console.WriteLine(dice2.Value);
+            Console.WriteLine(dice3.Value);
+            Console.WriteLine(dice4.Value);
+            Console.WriteLine(dice5.Value);
+
+            Console.WriteLine();
+
+            Die[] dice = new Die[5];
+            // create the dice
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+
+
+            // roll the dice
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i].Roll();
+                Console.WriteLine(dice[i].Value);
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ ConsoleApp1/die.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{   /// <summary>
+    /// Represents an individual 6 sided die
+    /// </summary>
+    class Die
+    {
+        // This static field is shared across all instances of the die class
+        private static Random rand = new Random();
+
+        /// <summary>
+        /// Creates a die with an initial random value
+        /// </summary>
+        public Die()
+        {
+            Roll();
+        }
+
+        /// <summary>
+        /// IsHeld = true if a die is held, false if not held
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Value = what the die is representing
+        /// </summary>
+        public byte Value { get; private set; }
+        // make set private, and force the class to generate random die values.
+
+        /// <summary>
+        /// Rolls the die if it is not held and sets the number between 1 and 6 inclusive
+        /// </summary>
+        public void Roll()
+        {
+            if (!IsHeld)
+            {
+                int randValue = rand.Next(1, 7);
+                this.Value = Convert.ToByte(randValue);
+            }
+            
+        } 
+    }
+}
